7dfb817f6b5fa1fe204c9f0e88fe86d040492f86
Imesh Ranawaka <imesh.20221787@iit.ac.lk>
2023-08-26 08:08:38 +0800

    rating page development done

--- MAD CW2/Controllers/User/RateMovieViewController.swift
+++ MAD CW2/Controllers/User/RateMovieViewController.swift
@@ -12,15 +12,24 @@ class RateMovieViewController: UIViewController {
 
     var selectedMovie: Movie?
     var context: NSManagedObjectContext!
+    var ratingIconViews = [UIImageView]()
+    var userMarkedRating: Int32 = 0
     
     @IBOutlet weak var movieTitleLbl: UILabel!
     @IBOutlet weak var movieGenreLbl: UILabel!
     @IBOutlet weak var movieRatingLbl: UILabel!
     @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var ratingView: UIStackView!
+    @IBOutlet weak var txtComment: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let ratingImageViews = ratingView.arrangedSubviews.compactMap { $0 as? UIImageView }
+        for ratingImg in ratingImageViews {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+            ratingImg.addGestureRecognizer(tapGesture)
+            ratingIconViews.append(ratingImg)
+        }
         loadMovie()
     }
     
@@ -30,7 +39,93 @@ class RateMovieViewController: UIViewController {
             movieGenreLbl.text = movie.genres!.replacingOccurrences(of: ",", with: " |")
             movieRatingLbl.text = String(describing: movie.rating)
             movieImg.image = CommonData.base64ToImage(movie.coverimage!)
+            
+            var userRating:Int32 = 0
+            let currentRating = getMovieRating()
+            if(currentRating != nil){
+                userRating = currentRating!.rating
+            }
+            updateRating(userRating: userRating)
         }
+    }
+    
+    func updateRating(userRating : Int32){
+        for (i, ratingImg) in ratingIconViews.enumerated() {
+            if(i < userRating){
+                ratingImg.image = UIImage(named: "YellowStarIcon")
+            }else{
+                ratingImg.image = UIImage(named: "FavouriteIcon")
+            }
+        }
+    }
+    
+    func getMovieRating() -> MovieRating?{
+        do{
+            if let movie = selectedMovie{
+                let request = NSFetchRequest<NSFetchRequestResult>(
+                    entityName: "MovieRating"
+                )
+                let predicate1 = NSPredicate(format: "userid == %@", UserDefaults.standard.string(forKey: String(describing: Enums.UserDefaultKeys.userId))!)
+                let predicate2 = NSPredicate(format: "movieid == %@", movie.id!)
+                let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [predicate1, predicate2])
+                request.predicate = compoundPredicate
+                request.fetchLimit = 1
+                let movieRating = try self.context?.fetch(request) as? [MovieRating]
+                if(movieRating != nil && movieRating!.count > 0){
+                    return movieRating![0]
+                }
+            }
+        }catch{
+            print("Retrieve movie movie rating failed.")
+        }
+        return nil;
+    }
+    
+    @objc private func handleTap(_ gesture: UITapGestureRecognizer) {
+        guard let index = ratingIconViews.firstIndex(of: gesture.view as! UIImageView) else { return }
+        userMarkedRating = Int32(index + 1)
+        updateRating(userRating: userMarkedRating)
+    }
+    
+    @IBAction func submitOnClick(_ sender: Any) {
+        do{
+            if let user = getUser(){
+                var movieRating = getMovieRating()
+                if(movieRating == nil){
+                    _ = MovieRating(comment: txtComment.text, id: UUID().uuidString, movieid: selectedMovie!.id!, rating: userMarkedRating, userid: UserDefaults.standard.string(forKey: String(describing: Enums.UserDefaultKeys.userId))!, movierelationship: selectedMovie!, userrelationship: user, insertIntoManagedObjectContext: context)
+                }else{
+                    movieRating?.rating = userMarkedRating
+                    movieRating?.comment = txtComment.text
+                }
+                
+                if((selectedMovie!.useroverallrating) == 0){
+                    selectedMovie!.useroverallrating = userMarkedRating
+                }else{
+                    selectedMovie?.useroverallrating = Int32((selectedMovie!.useroverallrating + userMarkedRating)/2)
+                }
+                try context.save()
+                navigationController?.popViewController(animated: true)
+            }
+        }catch{
+            print("Movie rating save failed.")
+        }
+    }
+    
+    func getUser() -> User?{
+        do{
+            let request = NSFetchRequest<NSFetchRequestResult>(
+                entityName: "User"
+            )
+            request.predicate = NSPredicate(format: "id == %@", UserDefaults.standard.string(forKey: String(describing: Enums.UserDefaultKeys.userId))!)
+            request.fetchLimit = 1
+            let users = try self.context?.fetch(request) as? [User]
+            if(users != nil && users!.count > 0){
+                return users![0]
+            }
+        }catch{
+            print("User fetch failed")
+        }
+        return nil
     }
     /*
     // MARK: - Navigation
